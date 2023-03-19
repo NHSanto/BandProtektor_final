8355b76389c969b100e6c543918c2f15d5f6a73c
NHSanto <nhsanto005@gmail.com>
2023-03-20 01:08:48 +0600

    add prettier in bothendand fix fontend

--- backend/src/controller/domainKeyWordController.ts
+++ backend/src/controller/domainKeyWordController.ts
@@ -1,15 +1,14 @@
-import {AppDataSource} from "../data-source";
-import {DomainKeyword} from "../entity/DomainKeyword";
+import { AppDataSource } from "../data-source";
+import { DomainKeyword } from "../entity/DomainKeyword";
 
 const domainKeywordRepository = AppDataSource.getRepository(DomainKeyword);
 
-const saveToDomainKeyWord = (domain,keyword,_user_name)=>{
-    const domainKeyword = Object.assign(new DomainKeyword(),{
-        domain,
-        keyword,
-        user_name:_user_name
-    })
-    return domainKeywordRepository.save(domainKeyword)
-}
-export  { saveToDomainKeyWord }
-
+const saveToDomainKeyWord = (domain, keyword, _user_name) => {
+  const domainKeyword = Object.assign(new DomainKeyword(), {
+    domain,
+    keyword,
+    user_name: _user_name,
+  });
+  return domainKeywordRepository.save(domainKeyword);
+};
+export { saveToDomainKeyWord };
